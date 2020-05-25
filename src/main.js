95d836d729c8f7fd14646d34827cba56ecaf09ae
Kamil Piech <kamil.piech.97@gmail.com>
2020-05-25 19:04:51 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -34,13 +34,13 @@ Vue.use(IconsPlugin);
 Vue.use(VueFlashMessage);
 
 const config = {
-  apiKey: 'AIzaSyB3vWU0XV5VuqRBrar-0J8JSAq9cf_kTwo',
-  authDomain: 'chatapp-da38a.firebaseapp.com',
-  databaseURL: 'https://chatapp-da38a.firebaseio.com',
-  projectId: 'chatapp-da38a',
-  storageBucket: 'chatapp-da38a.appspot.com',
-  messagingSenderId: '845839281342',
-  appId: '1:845839281342:web:12979703762298984102ef'
+  apiKey: '###',
+  authDomain: '###',
+  databaseURL: '###',
+  projectId: '###',
+  storageBucket: '###',
+  messagingSenderId: '###',
+  appId: '###'
 };
 
 
